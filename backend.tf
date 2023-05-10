05b2b2177f04c4fa548f3f9cd56d1c47f785ea23
kiriti07 <ch.kiriti@gmail.com>
2023-05-10 14:47:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "terraform-iaac-kiriti"
     key    = "terraform/s3/terraform.tfstate"
     region = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
+    //dynamodb_table = "terraform-state-lock"
     encrypt = true
   }
 }
