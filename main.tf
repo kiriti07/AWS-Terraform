bece81180d9ced43a419aaa6a1cf78a6e332fda5
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 00:46:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "App_instance" {
     Name = "Example Instance 1"
   }
 }
+resource "aws_key_pair" "terraform_ec2_key" {
+	key_name = "terraform_ec2_key"
+	public_key = file(".ssh/webapp-key.pub")
+}
