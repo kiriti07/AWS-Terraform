2f9387e0ac5e008a931b62956e8e7da024c85a67
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 13:41:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,41 @@
+resource "aws_key_pair" "example" {
+  key_name   = "keypair-webapp"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "example" {
+  name        = "securitygroup-webapp"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "App_instance" {
   ami           = "ami-0fdea1353c525c182"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  security_group_names = [aws_security_group.example.name]
 
   tags = {
     Name = "WebAPP"
   }
-}
-
-resource "aws_key_pair" "example" {
-  key_name   = "my-key-pair"
-  public_key = file("~/.ssh/id_rsa.pub")
 }
