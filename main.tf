2e53a793c0cd838caed5cb66af95be0f7a77cd85
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 14:26:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,43 +10,7 @@ resource "aws_instance" "App_instance" {
   }
 }
 
-  provisioner "local-exec" {
-    command = "mkdir -p ~/.ssh && touch ~/.ssh/id_rsa.pub"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod 600 ~/.ssh/id_rsa.pub"
-    ]
-  }
-
 resource "aws_key_pair" "example" {
   key_name   = "keypair-webapp"
   public_key = file("~/.ssh/id_rsa.pub")
-}
-
-resource "aws_security_group" "example" {
-  name        = "securitygroup-webapp"
-  description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
