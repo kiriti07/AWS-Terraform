b012a04e3a23c9e2a652533e32c6709b49da5c9a
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 13:30:06 +0530

    adding keypair

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "App_instance" {
     Name = "WebAPP"
   }
 }
+
+resource "aws_key_pair" "example" {
+  key_name   = "my-key-pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
