da0e2856375520e4b82e5ea763758d9ae10192e4
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 14:34:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "App_instance" {
   ami           = "ami-0fdea1353c525c182"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.example.key_name
-  security_group_names = [aws_security_group.example.name]
 
   tags = {
     Name = "WebAPP"
