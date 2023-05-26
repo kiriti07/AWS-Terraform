fdc9d6dbe4ce7a70b56f161b0f92ede810b43824
kiriti07 <ch.kiriti@gmail.com>
2023-05-26 15:34:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "App_instance" {
   ami           = "ami-0fdea1353c525c182"
   instance_type = "t2.micro"
-  key_name      = "Jenkins-CICD"
+  key_name      = "cicd-jenkins"
   connection {
     type = "ssh"
     user = "ec2-user"
     host = self.public_ip
-    private_key  = file("keypair/Jenkins-CICD.pem")
+    private_key  = file("keypair/cicd-jenkins.pem")
 }
 provisioner "remote-exec" {
   inline = [ 
@@ -14,19 +14,19 @@ provisioner "remote-exec" {
    ]
 }
   tags = {
-    Name = "WebApp"
+    Name = "App"
   }
 }
 
 resource "aws_instance" "Web_instance" {
   ami           = "ami-0fdea1353c525c182"
   instance_type = "t2.micro"
-  key_name      = "Jenkins-CICD"
+  key_name      = "cicd-jenkins"
   connection {
     type = "ssh"
     user = "ec2-user"
     host = self.public_ip
-    private_key  = file("keypair/Jenkins-CICD.pem")
+    private_key  = file("keypair/cicd-jenkins.pem")
 }
 provisioner "remote-exec" {
   inline = [ 
@@ -34,6 +34,6 @@ provisioner "remote-exec" {
    ]
 }
   tags = {
-    Name = "WebApp"
+    Name = "Web"
   }
 }
