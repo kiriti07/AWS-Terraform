405f1cb32f97ce258c49e625d3ae53b52304bb86
kiriti07 <ch.kiriti@gmail.com>
2023-05-26 15:21:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ provisioner "remote-exec" {
     Name = "WebApp"
   }
 }
+
+resource "aws_instance" "Web_instance" {
+  ami           = "ami-0fdea1353c525c182"
+  instance_type = "t2.micro"
+  key_name      = "Jenkins-CICD"
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+    private_key  = file("keypair/Jenkins-CICD.pem")
+}
+provisioner "remote-exec" {
+  inline = [ 
+    "sudo yum install tomcat -y"
+   ]
+}
+  tags = {
+    Name = "WebApp"
+  }
+}
